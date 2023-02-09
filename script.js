70a57230900831c797aaaf3f974394db62661eb2
Natalie <natalie.ac.hodson@gmail.com>
2023-02-09 17:18:15 +0200

    add comments to js

--- script.js
+++ script.js
@@ -3,30 +3,32 @@ let randomNum;
 let numGuesses= 0;
 let guesses = [];
 
-
+//generate random number, called on load
 function generateNum() {
     randomNum =  Math.floor(Math.random() * 100);
     const userGuess = document.querySelector('.guess');
     userGuess.focus();
 }
 
-
+//assign user's guess to array
+//show all guesses on screen
 function getGuess() {
     const userGuess = document.querySelector('.guess');
     const guessContainer = document.querySelector('.guesses');
     currentGuess = userGuess.value;
-
     if(!isNaN(currentGuess) && currentGuess <101){
         guesses.push(` ${currentGuess}`)
         guessContainer.textContent = guesses;
         numGuesses++;
     }
-
-
     userGuess.value = '';
     checkGuess();
 }
 
+//compare user's guess to generated number
+//show on screen whether it was too big, too small, or correct
+//if the number of guesses exceeds 10 then player loses
+//win or lose -> call function which shows reset button
 function checkGuess() {
     const userGuess = document.querySelector('.guess');
     const winOrLose = document.querySelector('.winOrLose');
@@ -36,7 +38,7 @@ function checkGuess() {
         winOrLose.textContent = `Please enter a number between 1 and 100.`
         userGuess.focus();
     } else {
-        if(numGuesses < 10){
+        if(numGuesses < 11){
             if(currentGuess == randomNum){
                 if(numGuesses > 1){
                     winOrLose.textContent = `You win! It took you ${numGuesses} guesses.`
@@ -62,17 +64,17 @@ function checkGuess() {
             resetButton()
         }    
     }
-
-
-    
-
 }
 
+//show reset button
 function resetButton() {
     const resetBut = document.querySelector('.resetBut');
     resetBut.style.display = 'block';
 }
 
+//when reset button is clicked, as the info from the previous game is removed
+//a new number is generated
+//game starts again.
 function resetGame() {
     const guessContainer = document.querySelector('.guesses');
     const winOrLose = document.querySelector('.winOrLose');
